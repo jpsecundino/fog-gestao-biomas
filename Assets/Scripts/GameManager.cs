8727322ce0c7c504c6c19ce7a9db89edbf35175f
Sfrev <51010839+Sfrev@users.noreply.github.com>
2020-09-21 00:30:09 -0300

    Early camera movement
    basic movement added but need improvements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] CameraMovement cameraMovement = null;
+    //[SerializeField] Transform manualMovementTransform = null;
+    [SerializeField] Text debugText;
+
+    private Vector3 offset = new Vector3(0f, 5f, -10f);
+    private Vector3 cameraFollowPosition;
+    private bool edgeScrolling = false;
+    private bool cameraCanMove = false;
+    private float cameraZoom;
+
+    private void Start()
+    {
+        cameraMovement.Setup(() => cameraFollowPosition + offset, () => cameraZoom);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            edgeScrolling = !edgeScrolling;
+            Debug.Log("Edge Scrolling:" + edgeScrolling);
+            debugText.text = "Debug(Press SPACE or right mouse button)\nCamera movement = " + cameraCanMove + "\nEdge movement = " + edgeScrolling;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            cameraCanMove = !cameraCanMove;
+            Debug.Log("Camera movement = " + cameraCanMove);
+            debugText.text = "Debug(Press SPACE or right mouse button)\nCamera movement = " + cameraCanMove + "\nEdge movement = " + edgeScrolling;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            cameraCanMove = !cameraCanMove;
+            Debug.Log("Camera movement = " + cameraCanMove);
+            debugText.text = "Debug(Press SPACE or right mouse button)\nCamera movement = " + cameraCanMove + "\nEdge movement = " + edgeScrolling;
+        }
+        if (cameraCanMove)
+        {
+            KeyboardMovement();
+        }
+        if (edgeScrolling)
+        {
+            EdgeMovement();
+        }
+    }
+
+    private void EdgeMovement()
+    {
+        float moveAmount = 50f;
+        float edgeSize = 35f;
+
+        //Right
+        if (Input.mousePosition.x > Screen.width - edgeSize)
+        {
+            cameraFollowPosition.x += moveAmount * Time.deltaTime;
+        }
+        //Left
+        if (Input.mousePosition.x < edgeSize)
+        {
+            cameraFollowPosition.x -= moveAmount * Time.deltaTime;
+        }
+        //Up
+        if (Input.mousePosition.y > Screen.height - edgeSize)
+        {
+            cameraFollowPosition.z += moveAmount * Time.deltaTime;
+        }
+        //Down
+        if (Input.mousePosition.y < edgeSize)
+        {
+            cameraFollowPosition.z -= moveAmount * Time.deltaTime;
+        }
+    }
+    private void KeyboardMovement()
+    {
+        float moveAmount = 50f;
+
+        //Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            cameraFollowPosition.x += moveAmount * Time.deltaTime;
+        }
+        //Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            cameraFollowPosition.x -= moveAmount * Time.deltaTime;
+        }
+        //Up
+        if (Input.GetKey(KeyCode.W))
+        {
+            cameraFollowPosition.z += moveAmount * Time.deltaTime;
+        }
+        //Down
+        if (Input.GetKey(KeyCode.S))
+        {
+            cameraFollowPosition.z -= moveAmount * Time.deltaTime;
+        }
+    }
+
+    private void CameraZoom()
+    {
+        float zoomChangeAmount = 1000f;
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            cameraZoom -= zoomChangeAmount * Time.deltaTime;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            cameraZoom += zoomChangeAmount * Time.deltaTime;
+        }
+
+        cameraZoom = Mathf.Clamp(cameraZoom, 40f, 100f);
+    }
+}
---
+++ Assets/Scripts/ManualMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
