220e74044991cae96138be3737c1fe8b12c0a2ab
jpsecundino <jpsecundino@users.noreply.github.com>
2020-09-29 17:24:19 -0300

    plant-soil interactions first prototype

---
+++ Assets/Scripts/Soil/Soil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soil : MonoBehaviour
+{
+
+    public float nutrientGenerationRate = 10f;
+    public float maxNutrients = 100f;
+    public float availableNutrients = 50f;
+
+    public List<Plant> plants;
+
+    [SerializeField] private float acidity;
+    [SerializeField] private float moisture;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GenerateNutrients();
+        GiveNutrients();
+    }
+
+    private void GiveNutrients()
+    {
+        foreach (Plant p in plants) {
+            p.Consume();
+        }
+    }
+
+    private void GenerateNutrients()
+    {
+        availableNutrients = Mathf.Clamp(availableNutrients + Time.deltaTime * nutrientGenerationRate, 0, maxNutrients);
+    }
+}
