49ad10eef504aadfded01a1aa3221b0018eebd9c
jpsecundino <39441841+jpsecundino@users.noreply.github.com>
2020-11-29 14:22:36 -0300

    soil can now generate nutrients inside the grid

--- Assets/Scripts/Soil/Nature.cs
+++ Assets/Scripts/Soil/Nature.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Nature : MonoBehaviour
+public partial class Nature : MonoBehaviour
 {
     #region Singleton
     public static Nature instance;
@@ -20,37 +20,19 @@ public class Nature : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     #endregion
-    /*
-    public float availableNutrients = 50f;
-    public float nutrientGenerationRate = 10f;
-    public float maxNutrients = 100f;
-    */
-
-    public struct Soil {
-        public float availableNutrients { get; set; }
-        public float nutrientGenerationRate { get; set; }
-        public float maxNutrients { get; set; }
-        //[SerializeField] private float acidity;
-        //[SerializeField] private float moisture;
-        public Soil(float _availableNutrients, float _nutrientGenerationRate, float _maxNutrients)
-        {
-            availableNutrients = _availableNutrients;
-            nutrientGenerationRate = _nutrientGenerationRate ;
-            maxNutrients = _maxNutrients ;
-        }
-
-    };
 
     [SerializeField] private float xInitialRegion = 0;
     [SerializeField] private float zInitialRegion = 0;
 
+    public static Action GenerateNutrients;
+    public float timeLoop;
+    private float actualTime;
+
     public static Dictionary<Vector3, Soil> soilGrid;
     private GridMap gridMap = null;
 
-    //a cada unidade de tempo, o solo se comunique:
-    // solo -> solo
-    // planta -> solo
     private void Start()
     {
         gridMap = GridMap.instance;
@@ -58,21 +40,34 @@ public class Nature : MonoBehaviour
         InitializeGrid();
     }
 
+    private void FixedUpdate()
+    {
+        actualTime += Time.deltaTime;
+
+        if(actualTime >= timeLoop)
+        {
+            GenerateNutrients();
+            actualTime = 0f;
+        }
+    }
     private void InitializeGrid()
     {
+        
         for (int i = 0; i < gridMap.xSize; i++)
         {
             for (int j = 0; j < gridMap.zSize; j++)
             {
+                //preeche uma porção inicial de solo
                 if (i <= xInitialRegion && j <= zInitialRegion)
                 {
-                    soilGrid.Add(new Vector3(i, 0, j), new Soil(100f, 10f, 100));
+                    Debug.Log("Inicializei o solo");
+                    soilGrid.Add(new Vector3(i, 0, j), new Soil(10f, 10f, 100));
                 }
                 else
                 {
+                    Debug.Log("Não Inicializei o solo");
                     soilGrid.Add(new Vector3(i, 0, j), new Soil(0, 0, 100));
                 }
-                //Debug.Log(soilGrid[new Vector3(i, 0, j)].availableNutrients);
             }
         }
     }
@@ -84,28 +79,8 @@ public class Nature : MonoBehaviour
 
     public void ConsumeNutrients(Vector3 pos, float consumeValue)
     {
-        Soil s = soilGrid[gridMap.GetNearestPointOnGrid(pos)];
-        s.availableNutrients = Mathf.Clamp(soilGrid[gridMap.GetNearestPointOnGrid(pos)].availableNutrients - consumeValue, 0, soilGrid[gridMap.GetNearestPointOnGrid(pos)].maxNutrients);
-        soilGrid[gridMap.GetNearestPointOnGrid(pos)] = s;
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        GenerateNutrients();
-        GiveNutrients();
+        soilGrid[gridMap.GetNearestPointOnGrid(pos)].GiveNutrients(consumeValue);
     }
 
-    private void GiveNutrients()
-    {
-    }
 
-    private void GenerateNutrients()
-    {
-
-    }
-
-    public void ConsumeFromSoil(Vector3 pos, float nutrients)
-    {
-
-    }
 }
