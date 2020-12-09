9e753f1e14b6c56901f2d73bba7b30365e866fa9
Sfrev <51010839+Sfrev@users.noreply.github.com>
2020-12-09 11:49:25 -0300

    Save System

---
+++ Assets/Scripts/Data/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SaveSystem
+{
+    public static void SaveGame(Nature nature, GridMap gridmap, ShopManager shopManager, InventoryManager inventoryManager)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/NOMEDOJOGO Save.bin";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        SaveData data = new SaveData(nature, gridmap, shopManager, inventoryManager);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static SaveData LoadGame()
+    {
+        string path = Application.persistentDataPath + "/NOMEDOJOGO Save.bin";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            SaveData data = formatter.Deserialize(stream) as SaveData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in" + path);
+            return null;
+        }
+    }
+}
